390069d5780b47be81b03f4a7d3d24c10621d2fc
victorlourencoo <victorlourenco443@gmail.com>
2020-05-12 11:22:37 -0300

    conexao mongodb

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/dbToDo';
+const connectToDataBase = async () => {
+const con = await mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+console.log('MongoDB conectado: ' $(con.Collection.host));
+
+}
+module.exports = mongoose;
