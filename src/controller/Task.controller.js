b5a24bcf32e80ca7dfa7cd737d10200968bc163e
victorlourencoo <victorlourenco443@gmail.com>
2020-05-13 23:36:58 -0300

    remover uma tarefa + configuração de rota

--- src/controller/Task.controller.js
+++ src/controller/Task.controller.js
@@ -54,6 +54,15 @@ class TaskController {
         return res.status(400).json(error, 'erro na busca da tarefa');
       });
   }
+  async delete(req, res) {
+    await TaskModel.deleteOne({ _id: req.params.id })
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error, 'Erro ao deletar tarefa');
+      });
+  }
 }
 
 module.exports = new TaskController();
--- src/routes/Task.Routes.js
+++ src/routes/Task.Routes.js
@@ -11,4 +11,5 @@ routes.post('/', TaskValidation, TaskController.create);
 routes.put('/:id', TaskValidation, TaskController.update);
 routes.get('/all', MacAddressValidation, TaskController.all);
 routes.get('/:id', TaskController.show);
+routes.delete('/:id', TaskController.delete);
 module.exports = routes;
