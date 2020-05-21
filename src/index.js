f798c6f234d3777effc5cb9049fb7afe91d288c5
victorlourencoo <victorlourenco443@gmail.com>
2020-05-21 01:17:52 -0300

    correção de params (mac)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const connectToDataBase = require('./config/database');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
-
+app.use(cors());
 const taskRoute = require('./routes/Task.Routes');
 
 app.use('/task', taskRoute);
